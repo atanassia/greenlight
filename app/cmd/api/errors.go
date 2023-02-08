8bb2f750f6036aaa8e607dd9c12726d8735aab40
Liza Babayan <liza_babayan.ru@mail.ru>
2023-02-08 14:57:24 +0300

    changed update handler and etc

--- app/cmd/api/errors.go
+++ app/cmd/api/errors.go
@@ -12,7 +12,7 @@ func (app *application) logError(r *http.Request, err error) {
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
 	env := envelope{"error": message}
 	err := app.writeJSON(w, status, env, nil)
-	
+
 	if err != nil {
 		app.logError(r, err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -41,4 +41,9 @@ func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 
 func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
+}
+
+func (app *application) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
 }
