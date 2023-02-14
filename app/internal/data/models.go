b105d9a5c269205cfaaf834e2f9848e14dbcea00
Liza Babayan <liza_babayan.ru@mail.ru>
2023-02-14 13:11:25 +0300

    created usermodel

--- app/internal/data/models.go
+++ app/internal/data/models.go
@@ -7,27 +7,17 @@ import (
 
 var (
 	ErrRecordNotFound = errors.New("record not found")
-	ErrEditConflict = errors.New("edit conflict")
+	ErrEditConflict   = errors.New("edit conflict")
 )
 
 type Models struct {
-	Movies interface {
-		Insert(movie *Movie) error
-		Get(id int64) (*Movie, error)
-		Update(movie *Movie) error
-		Delete(id int64) error
-		GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error)
-	}
+	Movies MovieModel
+	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
-	}
-}
-
-func NewMockModels() Models {
-	return Models{
-		Movies: MockMovieModel{},
+		Users:  UserModel{DB: db},
 	}
 }
